3b4d5c06293ca993047c3b8f05e5d0758c12cc76
VladimirImir <mimimvvv@gmail.com>
2023-06-19 20:03:19 +0300

    3.1 - Переменные.

---
+++ src/lesson3/Lesson3Example.java
@@ -0,0 +1,26 @@
+package lesson3;
+
+public class Lesson3Example {
+
+    public static void main(String[] args) {
+        int temp;
+        temp = 567;
+        System.out.println(temp);
+
+        int value = 25;
+        System.out.println(value);
+
+        double value2 = (78.12);
+        System.out.println(value2);
+
+        boolean b = true;
+        System.out.println(b);
+
+        char c = 'a';
+        System.out.println(c);
+
+        c = 't';
+        System.out.println(c);
+
+    }
+}
