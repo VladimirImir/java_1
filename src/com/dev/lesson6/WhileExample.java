22c51cf974562dfca05bf2f028f64c826da7572e
VladimirImir <mimimvvv@gmail.com>
2023-06-20 00:44:50 +0300

    6.1 - Цикл while.

---
+++ src/com/dev/lesson6/WhileExample.java
@@ -0,0 +1,22 @@
+package com.dev.lesson6;
+
+public class WhileExample {
+
+    public static void main(String[] args) {
+        int value = 0;
+        do {
+            System.out.println(value);
+            value++;
+        } while (value < 10);
+
+        //whileExample();
+    }
+
+    private static void whileExample() {
+        int value = 10;
+        while (value > 0) {
+            System.out.println(value);
+            value--;
+        }
+    }
+}
